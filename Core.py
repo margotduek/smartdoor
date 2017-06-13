ba0850f0500a1eae375fce292adb9e84ebc619cb
Jesse Berliner-Sachs <Jesse@Jesses-MacBook.local>
2017-06-13 19:00:14 +0300

    updating core.py

--- Core.py
+++ Core.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
 from gtts import gTTS
-
+import recieve_sms
 from twilio.rest import Client
-from django.core.exceptions import MiddlewareNotUsed
 import os
-import logging
 import json
 
 #import pyttsx
@@ -13,16 +11,16 @@ import json
 #engine.say('How are you.')
 #engine.runAndWait()  #start converting.this will automatically play the speech the response
 
+owners = []
+
 class AuthorizedUser():
     def __init__(self, name, owner, phoneNumber):
         self.name = name
         self.owner = owner
         self.preferences = []
         if owner:
-            with open('config/administrators.json', 'w') as adminsFile:
-                admins = json.load(adminsFile)
-                newOwner = {'phone number' : phoneNumber, 'name' : name}
-                admins.append(newOwner)
+            o = {'phone number': phoneNumber, 'name': name}
+            owners.append(o)
 
     def setUpHouse(self):
         for pref in self.preferences:
@@ -35,35 +33,36 @@ def Enter(usr):
     print 'Door Unlocked'
     usersHome.append(usr)
     usr.setUpHouse()
+    os.system("say Hi " + usr.name + ". Welcome home")
 
-    t = "Welcome Home" + usr.name
-    tts = gTTS(text=t, lang='en')
-    tts.save("welcome.mp3")
-    os.system("mpg321 welcome.mp3")
+def Deny():
+    print 'Door Locked'
+    os.system("say You have been denied entry. Please leave the premises immediately")
+
 
 def Leave(usr):
     usersHome.remove(usr)
 
-def unauthorizedUserAtDoor():
-    #MESSAGE = getKnockerName() + " is at your door. Do you want to let them in? Respond 'Yes' or 'No'"
-    MESSAGE = "Saul is at your door. Do you want to let them in? Respond 'Yes' or 'No'"
 
+def unauthorizedUserAtDoor():
     account_sid = "ACa87fd6d5a2c8d63346d60fc3399d036e"
-    # account_sid = "PNabca243db30e6e08dd0fc836d4d63f71"
     auth_token = "147c37d7b50171110f8996a67c0e0dab"
     client = Client(account_sid, auth_token)
+    for owner in owners:
+        toNumber = owner["phone number"]
+        text = "Hi " + owner['name']
+        body = text + ". " + getKnockerName() + " is at your door. Do you want to let them in? Respond 'Yes' or 'No'"
+        #message = client.api.account.messages.create(to=toNumber,
+        #                                     from_="+14159935014",
+        #                                     body=body,
+        #                                     media_url = [getKnockerImage()])
+        print 'sent message "' + body + '"'
+    letIn = recieve_sms.sms()
+    if letIn:
+        Enter(AuthorizedUser(getKnockerName(), False, None))
+    else:
+        Deny()
 
-    with open('config/administrators.json', 'r') as adminsFile:
-        admins = json.load(adminsFile)
-        print admins
-        for admin in admins:
-            toNumber = admin["phone_number"]
-            MESSAGE = getKnockerName() + " is at your door. Do you want to let them in? Respond 'Yes' or 'No'"
-            message = client.api.account.messages.create(to=toNumber,
-                                                 from_="+14159935014",
-                                                 body=MESSAGE,
-                                                 media_url = [getKnockerImage()])
-    print 'sent'
 
 def getKnockerImage():
     return "https://github.com/margotduek/smartdoor/raw/unstable/saul_pic.JPG"
@@ -74,11 +73,9 @@ def getKnockerName():
 authorizedUsers = []
 usersHome = []
 
-authorizedUsers[0] = AuthorizedUser('Jesse', True, "+972534264710")
+jesse = AuthorizedUser('Jesse', True, "+972534264710")
+authorizedUsers.append(jesse)
 
+Enter(jesse)
 unauthorizedUserAtDoor()
-
-
-
-
 
